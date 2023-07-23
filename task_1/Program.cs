59faec4a56650d5eabe19e8e5d93c9fd86ede197
AlexUnknown <saint.kaizer@yandex.ru>
2023-07-23 13:43:38 +0300

    .\task_1\

---
+++ task_1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальный элемент массива: ");
+int minRand = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальный элемент массива: ");
+int maxRand = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrixArray = GetMatrix(rows, colums, minRand, maxRand);
+PrintMatrix(matrixArray);
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            matrix[i, j] = new Random().Next(minRand, maxRand + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeRows (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(1) - 1; k++)
+            {
+                if (matr[i, k] < matr[i, k + 1])
+                {
+                    int temp = matr[i, k + 1];
+                    matr[i, k + 1] = matr[i, k];
+                    matr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив: ");
+ChangeRows(matrixArray);
+PrintMatrix(matrixArray);
